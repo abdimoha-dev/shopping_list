d1ef7c53267bd40d3e709b876f9dc967970a725d
abdy254 <abdymohammed@gmail.com>
2019-03-25 16:14:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: '',
+      quantity: ''
+    }
+  }
+
+  handle_item(event) {
+    this.setState({ item: event.target.value })
+
+  }
+  handle_quantity(event) {
+    this.setState({ quantity: event.target.value })
+
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="card" >
+          <div class="header">
+            <h1> Shopping List </h1>
+          </div>
+          <div className="container">
+            <ul id="cart">
+            </ul>
+          </div>
+          <div className="footer">
+            <h3>Total:</h3>
+            <span id="total-price"></span>
+          </div>
+        </div>
+        <div className="inputArea">
+          <input placeholder='enter the items to add' name='item' value={this.state.item} onChange={this.handle_item.bind(this)} type="text" id="item" />
+          <input placeholder='enter number of items' name='quantity' value={this.state.quantity} onChange={this.handle_quantity.bind(this)} type="text" id="price" />
+          <button>Add To Cart</button>
+        </div>
       </div>
     );
   }
