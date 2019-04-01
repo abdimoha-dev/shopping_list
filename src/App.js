2254ca13387efdada92a4a2b93dd44cd02af3a48
abdy254 <abdymohammed@gmail.com>
2019-04-01 12:26:52 +0300

    Modified user interface

--- src/App.js
+++ src/App.js
@@ -13,32 +13,37 @@ class App extends Component {
 
   handle_item(event) {
     this.setState({ item: event.target.value })
+    return this.state.item
 
   }
   handle_quantity(event) {
     this.setState({ quantity: event.target.value })
-
+    return this.state.quantity
+  }
+  add_to_cart(event) {
+    console.log('click to add to cart')
   }
   render() {
     return (
       <div className="App">
         <div className="card" >
-          <div class="header">
+          <div className="header">
             <h1> Shopping List </h1>
           </div>
           <div className="container">
             <ul id="cart">
+              <li><b>{this.state.item}</b></li>
+              <li><b>{this.state.quantity}</b></li>
             </ul>
           </div>
           <div className="footer">
-            <h3>Total:</h3>
             <span id="total-price"></span>
           </div>
         </div>
         <div className="inputArea">
           <input placeholder='enter the items to add' name='item' value={this.state.item} onChange={this.handle_item.bind(this)} type="text" id="item" />
           <input placeholder='enter number of items' name='quantity' value={this.state.quantity} onChange={this.handle_quantity.bind(this)} type="text" id="price" />
-          <button>Add To Cart</button>
+          <button onClick={this.add_to_cart.bind(this)}>Add To Cart</button>
         </div>
       </div>
     );
